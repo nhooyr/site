91c39ced4990127cd780f6ea415e33346327a6eb
Anmol Sethi <hi@nhooyr.io>
2020-06-29 02:34:01 -0400

    Time for a rewrite!

--- ci/lint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd "$(dirname "$0")/.."
-
-eslint --max-warnings=0 --fix $(git ls-files "*.ts" "*.tsx" "*.js")
-stylelint --fix $(git ls-files "*.js" "*.ts" "*.tsx")
--- src/svgr.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "*.svg" {
-  import React from "react"
-  const fc: React.FC<React.SVGProps<SVGSVGElement>>
-  export default fc
-}
